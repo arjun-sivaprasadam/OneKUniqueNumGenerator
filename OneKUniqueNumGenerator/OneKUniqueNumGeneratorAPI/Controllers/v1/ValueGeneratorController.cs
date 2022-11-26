bafb24a269d50d1dfcd1fe2f49117ac69ed26645
ArjunSivaprasadam <arjusivaprasadam@gmail.com>
2022-11-25 18:16:00 -0700

    Moving the controller to a subfolder to allow for future API versioning.

--- OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Controllers/v1/ValueGeneratorController.cs
+++ OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Controllers/v1/ValueGeneratorController.cs
@@ -2,7 +2,7 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace OneKUniqueNumGeneratorAPI.Controllers;
+namespace OneKUniqueNumGeneratorAPI.Controllers.v1;
 [Route("api/[controller]")]
 [ApiController]
 public class ValueGeneratorController : ControllerBase
