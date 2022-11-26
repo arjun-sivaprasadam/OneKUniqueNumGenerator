1ce48f52ddb3a94960fd052ab49d5848678010a6
ArjunSivaprasadam <arjusivaprasadam@gmail.com>
2022-11-25 19:04:33 -0700

    A quick test project before jumping to the web API!

---
+++ OneKUniqueNumGenerator/Test/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OneKUniqueNumGeneratorAPI.Logic;
+
+Console.WriteLine("Quick Test for testing edge cases!");
+var v = UniqueNumberGenerator.GetRandomNumbers();
+Console.WriteLine(v);
