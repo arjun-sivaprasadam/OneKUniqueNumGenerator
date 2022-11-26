3985ce8053c15d512db903b2faf002eb2df5e7c3
ArjunSivaprasadam <arjusivaprasadam@gmail.com>
2022-11-25 18:48:07 -0700

    Adding base logic for getting N unique numbers!

--- OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Logic/UniqueNumberGenerator.cs
+++ OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Logic/UniqueNumberGenerator.cs
@@ -4,10 +4,22 @@ public static class UniqueNumberGenerator
 {
     public static IEnumerable<ushort> GetUniqueNumbers(ushort count = 1000, ushort rangeMin = ushort.MinValue, ushort rangeMax = ushort.MaxValue)
     {
-        List<ushort> result = new List<ushort>();
+        List<ushort> values = new List<ushort>();
 
-        // TODO - Add core logic here!
+        for (int i = 0; i < count; i++)
+            AddUinqueUshortToList(ref values, rangeMin, rangeMax);
 
-        return result;
+        return values;
+    }
+
+    private static void AddUinqueUshortToList(ref List<ushort> values, ushort rangeMin, ushort rangeMax)
+    {
+        ushort v = (ushort)Random.Shared.Next(rangeMin, rangeMax);
+
+        if (!values.Contains(v))
+            values.Add(v);
+        else // the item is already present try again
+            AddUinqueUshortToList(ref values, rangeMin, rangeMax);
+
     }
 }
