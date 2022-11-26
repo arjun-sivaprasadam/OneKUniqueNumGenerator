33cc7ff462bd92fcbc01a83dffdd9e330f1f532b
ArjunSivaprasadam <arjusivaprasadam@gmail.com>
2022-11-25 18:32:32 -0700

    Adding a skeleton function! The function is created in the intention that it may get scaled in the future.

---
+++ OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Logic/UniqueNumberGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace OneKUniqueNumGeneratorAPI.Logic;
+
+public static class UniqueNumberGenerator
+{
+    public static IEnumerable<ushort> GetUniqueNumbers(ushort count = 1000, ushort rangeMin = ushort.MinValue, ushort rangeMax = ushort.MaxValue)
+    {
+        List<ushort> result = new List<ushort>();
+
+        // TODO - Add core logic here!
+
+        return result;
+    }
+}
