71fd638488f16cdf9fd424a4fd0e159c5e364a80
ArjunSivaprasadam <arjusivaprasadam@gmail.com>
2022-11-25 19:25:35 -0700

    Creating an initial revision of API!

--- OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Controllers/v1/ValueGeneratorController.cs
+++ OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Controllers/v1/ValueGeneratorController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using OneKUniqueNumGeneratorAPI.Logic;
 
 namespace OneKUniqueNumGeneratorAPI.Controllers.v1;
+
 [Route("api/v1/[controller]")]
 [ApiController]
 public class ValueGeneratorController : ControllerBase
 {
     // GET: api/v1/ValueGenerator
+    /// <summary>
+    /// This API will generate and provide 1000 random numbers ranging in between 0 and 65535.
+    /// </summary>
+    /// <returns>List of 1k random numbers</returns>
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public ActionResult<IEnumerable<ushort>> Get()
     {
-        return Ok(new string[] { "value1", "value2" });
+        try
+        {
+            return Ok(UniqueNumberGenerator.GetRandomNumbers());
+        }
+        catch (Exception)
+        {
+            // TODO - Logic to log this exception to logfile.
+
+            return BadRequest();
+        }
     }
 }
