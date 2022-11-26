67a6792379ac69ac08e2626d0f48eecf5a9447a4
ArjunSivaprasadam <arjusivaprasadam@gmail.com>
2022-11-25 18:19:36 -0700

    More to support for future API versions. This is will a custom URL for API access based on the API version.

--- OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Controllers/v1/ValueGeneratorController.cs
+++ OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Controllers/v1/ValueGeneratorController.cs
@@ -3,11 +3,11 @@
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace OneKUniqueNumGeneratorAPI.Controllers.v1;
-[Route("api/[controller]")]
+[Route("api/v1/[controller]")]
 [ApiController]
 public class ValueGeneratorController : ControllerBase
 {
-    // GET: api/<ValueGeneratorController>
+    // GET: api/v1/ValueGenerator
     [HttpGet]
     public IEnumerable<string> Get()
     {
