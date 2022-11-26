ef440de1c620b242c9cfd1c20661f58e41402c1b
ArjunSivaprasadam <arjusivaprasadam@gmail.com>
2022-11-25 18:13:33 -0700

    Removing all other HTTP methods which are out of the scope of development requirements!

--- OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Controllers/ValueGeneratorController.cs
+++ OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Controllers/ValueGeneratorController.cs
@@ -13,29 +13,4 @@ public class ValueGeneratorController : ControllerBase
     {
         return new string[] { "value1", "value2" };
     }
-
-    // GET api/<ValueGeneratorController>/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-        return "value";
-    }
-
-    // POST api/<ValueGeneratorController>
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-    }
-
-    // PUT api/<ValueGeneratorController>/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/<ValueGeneratorController>/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
 }
