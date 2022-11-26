a6a409be1a947c17e04397b48b486f1997187a55
ArjunSivaprasadam <arjusivaprasadam@gmail.com>
2022-11-25 18:08:45 -0700

    Adding a template controller named as ValuegeneratorController.cs!

---
+++ OneKUniqueNumGenerator/OneKUniqueNumGeneratorAPI/Controllers/ValueGeneratorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OneKUniqueNumGeneratorAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ValueGeneratorController : ControllerBase
+{
+    // GET: api/<ValueGeneratorController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<ValueGeneratorController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<ValueGeneratorController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<ValueGeneratorController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<ValueGeneratorController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
